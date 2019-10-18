f531301f200238a8a7d48cfdf7d4e70d71a02a78
Francisco Contreras <francisco@sgwinc.net>
2019-10-18 10:30:52 -0700

    ComputeAreaWithConstant

--- src/Chapter2/ComputeAReaWIthCOnstant.java
+++ src/Chapter2/ComputeAReaWIthCOnstant.java
@@ -2,5 +2,20 @@ package Chapter2;
 import java.util.Scanner;
 
 public class ComputeAReaWIthCOnstant {
-    public static void main(String[ args])
+    public static void main(String[] args) {
+        final double PI = 3.14159; // Dec;are a constant
+
+        // Create a Scanner object
+        Scanner input = new Scanner(System.in);
+
+        // Prompt the user to enter a radius
+        System.out.print("Enter a number for radius: ");
+        double radius = input.nextDouble();
+
+        // Compute area
+        double area = radius * radius * PI;
+
+        // Display the result
+        System.out.println("The area for the circle of radius " + radius + " is " + area);
+    }
 }
