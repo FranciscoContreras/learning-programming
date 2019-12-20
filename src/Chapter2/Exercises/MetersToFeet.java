2e81b9b263dd340140b8600a4fac9d79ff3cfa9e
FranciscoContreras <contrerasfrancisco2010@gmail.com>
2019-12-20 10:00:11 -0800

    Converting Meters to Feet

--- src/Chapter2/Exercises/MetersToFeet.java
+++ src/Chapter2/Exercises/MetersToFeet.java
@@ -1,5 +1,19 @@
 package Chapter2.Exercises;
-
+import java.util.Scanner;
 
 public class MetersToFeet {
+    public static void main(String[] args) {
+        // Create Scanner
+        Scanner input = new Scanner(System.in);
+
+        // Prompt user to enter a value
+        System.out.print("Enter a value for meter: ");
+        double meters = input.nextDouble();
+
+        // Compute for results
+        double feet = meters * 3.2786;
+
+        // Display Results
+        System.out.println(meters + " meters is " + feet + " feet." );
+    }
 }
