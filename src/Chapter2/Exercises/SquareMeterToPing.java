d4d77f6ee115ef5e5916d5323f1fa03f25b3d6c0
Francisco <contrerasfrancisco@icloud.com>
2019-12-21 13:03:20 -0800

    Converting squared meters to pings

--- src/Chapter2/Exercises/SquareMeterToPing.java
+++ src/Chapter2/Exercises/SquareMeterToPing.java
@@ -1,4 +1,20 @@
 package Chapter2.Exercises;
+import java.util.Scanner;
 
 public class SquareMeterToPing {
+public static void main (String[] args) {
+    // Create scanner
+    Scanner input = new Scanner(System.in);
+
+    // Prompt user to enter value
+    System.out.print("Enter a number in square meters: ");
+
+    double squareMeters = input.nextDouble();
+
+    // Compute for pings
+    double pings = squareMeters * 0.3025;
+
+    // Display results
+    System.out.println(squareMeters + " square meters is " + pings + " pings");
+}
 }
